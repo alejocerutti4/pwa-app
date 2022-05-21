e022671e97c9aa1e478b25f5886fb01e93862d22
Alejo Cerutti <alejocerutti4@gmail.com>
2022-05-20 23:40:28 -0300

    first-commit

---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+      <>
+        <main>
+          <h2>¿Quién soy?</h2>
+          <p>Un simple desarrollador de software</p>
+        </main>
+      </>
+    );
+  }
